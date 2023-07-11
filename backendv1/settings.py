26d0f11b79ed8d8256a0ce51b1a2261383253ceb
Yeranis Corrales Carmenate <ycorrales2016@fau.edu>
2023-07-11 12:15:42 -0400

    new setting

--- backendv1/settings.py
+++ backendv1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-pyo_m&a+yt#$iqexw8u5ud89yy9#h#jj0_hd_v+wg&i59zp^4%"
+SECRET_KEY = "*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
